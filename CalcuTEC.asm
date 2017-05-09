ed31c29b2ab28ad633a380d0e455aca241312175
liang11 <rliang11@hotmail.com>
2017-05-08 23:33:22 -0600

    Add files via upload

--- CalcuTEC.asm
+++ CalcuTEC.asm
@@ -5,15 +5,18 @@
 	bienvenido	db	'¡Bienvenido a la calculadora más crack del TEC!',10,13,0
 	sim			db	10,13,'$ ',0
 	msg_error	db	'Expresion Invalida!',0
-	operacion	db	'(6+2)*3/2+2-8 ',0;'(6+2)*3/2-4',0
+	operacion	db	'(6+2)*3/2+2-8',0;'B1010',0
 
 .UDATA
 	;operacion	resb	16
 	posfijo		resb	32
-	boolean		resb	1
-	aux			resd	1
-	var			resb	1
+	boolean		resb	1	;variable booleana
+	aux			resd	2	;auxiliar del ptro del posfijo
+	aux2		resd	1	;auxiliar al contador de la pila
+	var			resb	16	;numero en diferente base
+	base		resb	16
 	resultado	resd	16
+	auxB		resb	16
 	basura		resd	3
 
 .CODE
@@ -23,6 +26,10 @@
 		nwln				
 		PutStr sim					;imprimo simbolo de $
 		;GetStr operacion			;espero el inout del usuario y lo guardo en operacion 
+		
+		call verificarSintaxis
+		cmp byte[boolean], 1
+		je error
 		
 		generarPosfijo:
 			mov bl, [operacion+edx];muevo al ebx el 1er caracter de operacion
@@ -42,8 +49,8 @@
 			call verificarLetra		;verifico si es una letra
 			cmp byte[boolean], 1
 			je meterPosfijo
-			jmp error
-			
+			jmp verificarBase		;verifico si esta en otra base
+;-----------------------------------------------------------------------		
 		verificarPila:
 			cmp ecx, 0
 			je meterPila
@@ -82,9 +89,9 @@
 			je cambiarTope
 			cmp al, '/'
 			je cambiarTope
-			
+;-----------------------------------------------------------------------		
 		meterPila:
-			PutCh 'M'
+			;PutCh 'M'
 			Push ebx
 			inc ecx
 			mov eax, [aux]
@@ -101,7 +108,7 @@
 			
 		
 		meterPosfijo:
-			PutCh 'P'
+			;PutCh 'P'
 			mov [posfijo+eax], bl
 			inc eax
 			mov [aux], eax
@@ -123,10 +130,54 @@
 			disminuir:
 				dec ecx
 				jmp generarPosfijo
+;-----------------------------------------------------------------------		
+		verificarBase:
+			cmp bl, 'B'
+			je binario
+			cmp bl, 'O'
+			je octal
+			cmp bl, 'H'
+			je hexadecimal
+			jmp error
 		
+		binario:
+			mov [aux2], ecx
+			xor ecx, ecx
+			call contarCaracteres
+			;PutCh 'c';B1010
+			PutInt cx
+			;nwln
+			
+			mov eax, 0
+			
+			
+			a_auxB:
+				dec ecx
+				cmp ecx, 0
+				jl fi
+				mov bl, [var+ecx]
+				mov [auxB+ecx-1],bl
+				inc eax
+				jmp a_auxB
+			
+			fi:
+			BinToInt auxB,eax
+			;PutCh 'i'
+			PutLInt [aux]
+			mov ecx, [aux2]
+			mov bl, [var]
+			mov [posfijo+edx], bl
+			jmp generarPosfijo
+			
+		octal:
+		 
+		hexadecimal:
+		 
+;-----------------------------------------------------------------------	
 		error:
 			nwln
 			PutStr msg_error
+			.EXIT
 			
 		terminar:
 			nwln
@@ -169,41 +220,46 @@
 			je multiplicar
 			
 		sumar:
-			PutCh 'S'
+			;PutCh 'S'
+			;nwln
 			add eax, ebx
 			Push eax
-			PutInt ax
+			;PutInt ax
 			dec ecx
 			jmp resolver
 		
 		restar:
-			PutCh 'R'
+			;PutCh 'R'
+			;nwln
 			sub eax, ebx
 			Push eax
-			PutInt ax
+			;PutInt ax
 			dec ecx
 			jmp resolver
 			
 		dividir:
-			PutCh 'D'
+			;PutCh 'D'
+			;nwln
 			div bl				;-------------------- nop hacer diviciones cn tamanio mas q bl
 			Push eax
-			PutInt ax
+			;PutInt ax
 			dec ecx
 			jmp resolver
 			
 		multiplicar:
-			PutCh 'M'
+			;PutCh 'M'
+			;nwln
 			mul ebx
 			Push eax
-			PutInt ax
+			;PutInt ax
 			dec ecx
 			jmp resolver
 	
 		mostrarResultado: 
-			nwln
+			;nwln
 			Pop eax
 			;mov [var], eax
+			PutCh '='
 			PutInt ax
 			adios:
 			.EXIT
@@ -232,7 +288,8 @@ verificarOperacion:
 		ret
 	esOperacion:
 		mov byte[boolean], 1
-		PutCh 'O'
+		;PutCh 'O'
+		;nwln
 		ret
 	suma:
 		mov bl, [operacion+edx]
@@ -262,7 +319,8 @@ verificarNumero:
 	jge noNumero
 	esNumero:
 		mov byte[boolean], 1
-		PutCh 'N'
+		;PutCh 'N'
+		;nwln
 		ret
 	noNumero:
 		mov byte[boolean], 0
@@ -275,12 +333,33 @@ verificarLetra:
 	jge noLetra
 	esLetra:
 		mov byte[boolean], 1
-		PutCh 'L'
+		;PutCh 'L'
+		;nwln
 		ret
 	noLetra:
 		mov byte[boolean], 0
 		ret
+
+contarCaracteres:
+	mov bl, [operacion+edx]
+	call verificarOperacion
+	cmp byte[boolean], 1
+	je salir
+	cmp bl, '('
+	je salir
+	cmp bl, ')'
+	je salir
+	cmp bl, 0
+	je salir
+	mov [var+ecx], bl
+	PutCh bl
+	inc ecx
+	inc edx
+	jmp contarCaracteres
+	salir:
+		ret
 	
+verificarSintaxis:
 	
 ;HexToInt var,32
 	;mov eax,var
